26709c11a856b256a54e8efd9de514ffc2932c33
HERNAN DARIO CORDOBA GALLEGO <hernancordoba177819@correo.itm.edu.co>
2019-09-15 23:20:50 -0500

    Create DataContext

---
+++ HelpMoto.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using HelpMoto.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMoto.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Owner> Owners  { get; set; }
+    }
+}

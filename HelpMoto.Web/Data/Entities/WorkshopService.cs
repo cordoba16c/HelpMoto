db40dff19796f56e838e11c4c2028fa156959b90
HERNAN DARIO CORDOBA GALLEGO <hernancordoba177819@correo.itm.edu.co>
2019-09-21 14:08:33 -0500

    Create Entities

---
+++ HelpMoto.Web/Data/Entities/WorkshopService.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class WorkshopService
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Workshop Service ")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Name { get; set; }
+    }
+}

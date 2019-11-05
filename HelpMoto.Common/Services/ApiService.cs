3a5264a5f51933484c5d2ca8acea4ea44529cff5
Fabian <fabianrm6@gmail.com>
2019-11-04 21:24:58 -0500

    Add models and services for obtain token and owner information

--- HelpMoto.Common/Services/ApiService.cs
+++ HelpMoto.Common/Services/ApiService.cs
@@ -1,6 +1,9 @@
-﻿using Plugin.Connectivity;
+﻿using HelpMoto.Common.Models;
+using Newtonsoft.Json;
+using Plugin.Connectivity;
 using System;
-using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +19,100 @@ namespace HelpMoto.Common.Services
             }
 
             return await CrossConnectivity.Current.IsRemoteReachable(url);
+        }
+
+        public async Task<Response> GetTokenAsync(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            TokenRequest request)
+        {
+            try
+            {
+                var requestString = JsonConvert.SerializeObject(request);
+                var content = new StringContent(requestString, Encoding.UTF8, "application/json");
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase)
+                };
+
+                var url = $"{servicePrefix}{controller}";
+                var response = await client.PostAsync(url, content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                var token = JsonConvert.DeserializeObject<TokenResponse>(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = token
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public async Task<Response> GetOwnerByEmailAsync(
+            string urlBase,
+            string servicePrefix,
+            string controller,
+            string tokenType,
+            string accessToken,
+            string email)
+        {
+            try
+            {
+                var request = new EmailRequest { Email = email };
+                var requestString = JsonConvert.SerializeObject(request);
+                var content = new StringContent(requestString, Encoding.UTF8, "application/json");
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri(urlBase)
+                };
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(tokenType, accessToken);
+                var url = $"{servicePrefix}{controller}";
+                var response = await client.PostAsync(url, content);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                var owner = JsonConvert.DeserializeObject(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = owner
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
         }
     }
 }

5df0e106a124e981a5d78a1237e83152cfc2452d
JOHANA ANDREA GONZALEZ CARDONA <johanagonzalez187968@correo.itm.edu.co>
2019-11-07 20:51:01 -0500

    add style

---
+++ HelpMoto.Web/Models/HistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HelpMoto.Web.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpMoto.Web.Models
+{
+    public class HistoryViewModel : History
+    {
+        public int MotorcycleId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [Display(Name = "Workshop Type")]
+        [Range(1, int.MaxValue, ErrorMessage = "You must select a service type.")]
+        public int ServiceTypeId { get; set; }
+
+       /* public IEnumerable<SelectListItem> WorkshopType { get; set; }*/
+    }
+}

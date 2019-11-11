8e7d6c7a6c2f056611713cdf5bdd36d2f64c6553
JOHANA ANDREA GONZALEZ CARDONA <johanagonzalez187968@correo.itm.edu.co>
2019-11-11 11:32:23 -0500

    CRUD Motorcycle

--- HelpMoto.Web/Controllers/AccountController.cs
+++ HelpMoto.Web/Controllers/AccountController.cs
@@ -146,22 +146,24 @@ namespace HelpMoto.Web.Controllers
                 _dataContext.Owners.Add(owner);
                 await _dataContext.SaveChangesAsync();
 
-                var myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
-                var tokenLink = Url.Action("ConfirmEmail", "Account", new
+                var loginViewModel = new LoginViewModel
                 {
-                    userid = user.Id,
-                    token = myToken
-                }, protocol: HttpContext.Request.Scheme);
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
 
-                _mailHelper.SendMail(model.Username, "Email confirmation", $"<h1>Email Confirmation</h1>" +
-                            $"To allow the user, " +
-                            $"plase click in this link:</br></br><a href = \"{tokenLink}\">Confirm Email</a>");
-                ViewBag.Message = "The instructions to allow your user has been sent to email.";
-                return View(model);
+                var result2 = await _userHelper.LoginAsync(loginViewModel);
+
+                if (result2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
             }
 
             return View(model);
         }
+
 
         private async Task<User> AddUserAsync(AddUserViewModel model)
         {
@@ -269,29 +271,6 @@ namespace HelpMoto.Web.Controllers
 
             return View(model);
         }
-
-        public async Task<IActionResult> ConfirmEmail(string userId, string token)
-        {
-            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(token))
-            {
-                return NotFound();
-            }
-
-            var user = await _userHelper.GetUserByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var result = await _userHelper.ConfirmEmailAsync(user, token);
-            if (!result.Succeeded)
-            {
-                return NotFound();
-            }
-
-            return View();
-        }
-
         public IActionResult RecoverPassword()
         {
             return View();

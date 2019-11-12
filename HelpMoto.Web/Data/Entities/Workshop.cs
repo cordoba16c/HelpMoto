3c1e464c1e126c804ded4f5417b5295c49968fa7
JOHANA ANDREA GONZALEZ CARDONA <johanagonzalez187968@correo.itm.edu.co>
2019-11-11 23:33:49 -0500

    workshop

--- HelpMoto.Web/Data/Entities/Workshop.cs
+++ HelpMoto.Web/Data/Entities/Workshop.cs
@@ -18,6 +18,7 @@ namespace HelpMoto.Web.Data.Entities
         [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         public string Address { get; set; }
 
+
         [Display(Name = "Contact Name")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
